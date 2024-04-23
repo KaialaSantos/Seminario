5348f4e48e1efe135e2e1b8f5fbb879b94e2860b
KaialaSantos <167907916+KaialaSantos@users.noreply.github.com>
2024-04-23 15:12:47 -0300

    Update log calculadora.c
    adicionada funções:
    logaritmo

--- calculadora.c
+++ calculadora.c
@@ -5,6 +5,23 @@
 #include <math.h>
 
 int main() {
+  int operação;
+  double base, nlog, total;
+
+  printf ("1: Adição \n2: Subtração \n3: Multiplicação \n4: Divisão \n5: Potenciação \n6:Logaritmo ");
+  scanf("%d", (&operação));
+
+  if (operação == 6) {
+    printf("Insira a base do logaritmo: ");
+    scanf("%lf", &base);
+
+    printf("Insira o número: ");
+    scanf("%lf", &nlog);
+
+    double total = log(n1) / log(base);
+
+    printf("Resultado do log: %.2lf \n", total);
+  }
   
   return 0;
 }
